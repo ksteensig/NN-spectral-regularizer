80c3ace589ac822ffedf4b38d97c082cd85bed30
Kasper Steensig <kasper.steensig@gmail.com>
2020-03-09 17:43:13 +0100

    Put training data on GPU

--- train.py
+++ train.py
@@ -4,12 +4,14 @@ trainset = torchvision.datasets.MNIST(root='./data',
                                       train=True,
                                       download=True,
                                       transform=transforms.ToTensor())
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, pin_memory=True)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=128,
+                                          pin_memory=True)
 
 net = Net()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-net.to(device)
+net.to(device, non_blocking=True)
 
 print(net)
 
@@ -17,11 +19,13 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 # train network
-for epoch in range(20):  # loop over the dataset multiple times
+for epoch in range(1):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        print(inputs.size())
 
         # zero the parameter gradients
         optimizer.zero_grad()
