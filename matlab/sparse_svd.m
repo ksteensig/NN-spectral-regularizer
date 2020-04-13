728d1ad84b90fdac402ec6a3aab4aec88cd3309d
Kasper Steensig <kasper.steensig@gmail.com>
2020-04-13 12:17:26 +0200

    Remove batch processing from MATLAB sparse_svd

--- matlab/sparse_svd.m
+++ matlab/sparse_svd.m
@@ -1,23 +1,24 @@
-function [S] = sparse_svd(X)
-    sizeX = size(X{1});
-    m = sizeX(1);
-    n = sizeX(2);
+function [U,S,V] = sparse_svd(X)
+    sz = size(X);
+    m = sz(1);
+    n = sz(2);
     k = floor(n/10);
     p = 10;
-    l = k+p;
+    l = k + p;
 
-    Phi = randi([0 1],l,m);
-    S = cell(1,length(X));
-
-    % algorithm
-    for i=1:length(X)
-        Y = Phi*X{i};
-        B = Y*Y';
-        B = 0.5*(B + B');
-        [T,D] = eigs(B,k);
-        St = sqrt(D);
-        Vt = Y'*T/St;
-        Ut = X{i}*Vt;
-        S{i} = svd(Ut);
-    end
+    %index = randsample(1:length(X), l);
+    %Y = X(index, :);
+    
+    Phi = randn(l, m);
+    Y = Phi*X;
+    
+    B= Y*Y';
+    B = 0.5*(B + B');    
+    [T,D] = eigs(B,k);
+    St = sqrt(D);
+    Vt = Y'*T/St;
+    Ut = X*Vt;
+    [U,S,Q] = svd(Ut, 'econ');
+    %S = diag(S);
+    V = Vt*Q';
 end
